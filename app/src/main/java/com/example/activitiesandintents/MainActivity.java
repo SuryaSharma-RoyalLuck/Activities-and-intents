e9252ac45a77c09da4cf5fa862b189a8ee696829
SuryaSharma-RoyalLuck <ssurya16@tbc.edu.np>
2021-03-17 22:33:06 +0545

    Task-2 Completed

--- app/src/main/java/com/example/activitiesandintents/MainActivity.java
+++ app/src/main/java/com/example/activitiesandintents/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.activitiesandintents;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -16,8 +17,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+
     public void launchSecondActivity(View view)
     {
         Log.d(LOG_TAG, "Button clicked!");
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
     }
 }
